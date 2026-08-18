e0335daf716969817407d1aebccde95c7f5230a0
Peyton-fly <pengfei.tang@seeed.cc>
2026-08-18 18:38:42 +0800

    feat: merge NotoSansSC Medium CJK glyphs into 320x240 UI fonts

---
+++ generated/ui_320x240/ui_font_fallback_12.c
@@ -0,0 +1,123 @@
+/*******************************************************************************
+ * Size: 12 px
+ * Bpp: 4
+ * Opts: --font NotoSansSC-Medium.ttf --range 0x25A1 --size 12 --bpp 4 --no-compress --format lvgl --lv-font-name ui_font_fallback_12 -o out/ui_font_fallback_12.c
+ ******************************************************************************/
+
+#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+#include "lvgl.h"
+#else
+#include "lvgl/lvgl.h"
+#endif
+
+#ifndef UI_FONT_FALLBACK_12
+#define UI_FONT_FALLBACK_12 1
+#endif
+
+#if UI_FONT_FALLBACK_12
+
+/*-----------------
+ *    BITMAPS
+ *----------------*/
+
+/*Store the image of the glyphs*/
+static LV_ATTRIBUTE_LARGE_CONST const uint8_t glyph_bitmap[] = {
+    /* U+25A1 "□" */
+    0xa7, 0x77, 0x77, 0x77, 0x7a, 0x70, 0x0, 0x0,
+    0x0, 0x7, 0x70, 0x0, 0x0, 0x0, 0x7, 0x70,
+    0x0, 0x0, 0x0, 0x7, 0x70, 0x0, 0x0, 0x0,
+    0x7, 0x70, 0x0, 0x0, 0x0, 0x7, 0x70, 0x0,
+    0x0, 0x0, 0x7, 0x70, 0x0, 0x0, 0x0, 0x7,
+    0x70, 0x0, 0x0, 0x0, 0x7, 0xa7, 0x77, 0x77,
+    0x77, 0x7a, 0x0, 0x0, 0x0, 0x0, 0x0
+};
+
+
+/*---------------------
+ *  GLYPH DESCRIPTION
+ *--------------------*/
+
+static const lv_font_fmt_txt_glyph_dsc_t glyph_dsc[] = {
+    {.bitmap_index = 0, .adv_w = 0, .box_w = 0, .box_h = 0, .ofs_x = 0, .ofs_y = 0} /* id = 0 reserved */,
+    {.bitmap_index = 0, .adv_w = 192, .box_w = 10, .box_h = 11, .ofs_x = 1, .ofs_y = -1}
+};
+
+/*---------------------
+ *  CHARACTER MAPPING
+ *--------------------*/
+
+
+
+/*Collect the unicode lists and glyph_id offsets*/
+static const lv_font_fmt_txt_cmap_t cmaps[] =
+{
+    {
+        .range_start = 9633, .range_length = 1, .glyph_id_start = 1,
+        .unicode_list = NULL, .glyph_id_ofs_list = NULL, .list_length = 0, .type = LV_FONT_FMT_TXT_CMAP_FORMAT0_TINY
+    }
+};
+
+
+
+/*--------------------
+ *  ALL CUSTOM DATA
+ *--------------------*/
+
+#if LVGL_VERSION_MAJOR == 8
+/*Store all the custom data of the font*/
+static  lv_font_fmt_txt_glyph_cache_t cache;
+#endif
+
+#if LVGL_VERSION_MAJOR >= 8
+static const lv_font_fmt_txt_dsc_t font_dsc = {
+#else
+static lv_font_fmt_txt_dsc_t font_dsc = {
+#endif
+    .glyph_bitmap = glyph_bitmap,
+    .glyph_dsc = glyph_dsc,
+    .cmaps = cmaps,
+    .kern_dsc = NULL,
+    .kern_scale = 0,
+    .cmap_num = 1,
+    .bpp = 4,
+    .kern_classes = 0,
+    .bitmap_format = 0,
+#if LVGL_VERSION_MAJOR == 8
+    .cache = &cache
+#endif
+};
+
+
+
+/*-----------------
+ *  PUBLIC FONT
+ *----------------*/
+
+/*Initialize a public general font descriptor*/
+#if LVGL_VERSION_MAJOR >= 8
+const lv_font_t ui_font_fallback_12 = {
+#else
+lv_font_t ui_font_fallback_12 = {
+#endif
+    .get_glyph_dsc = lv_font_get_glyph_dsc_fmt_txt,    /*Function pointer to get glyph's data*/
+    .get_glyph_bitmap = lv_font_get_bitmap_fmt_txt,    /*Function pointer to get glyph's bitmap*/
+    .line_height = 11,          /*The maximum line height required by the font*/
+    .base_line = 1,             /*Baseline measured from the bottom of the line*/
+#if !(LVGL_VERSION_MAJOR == 6 && LVGL_VERSION_MINOR == 0)
+    .subpx = LV_FONT_SUBPX_NONE,
+#endif
+#if LV_VERSION_CHECK(7, 4, 0) || LVGL_VERSION_MAJOR >= 8
+    .underline_position = -1,
+    .underline_thickness = 1,
+#endif
+    .dsc = &font_dsc,          /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
+#if LV_VERSION_CHECK(8, 2, 0) || LVGL_VERSION_MAJOR >= 9
+    .fallback = NULL,
+#endif
+    .user_data = NULL,
+};
+
+
+
+#endif /*#if UI_FONT_FALLBACK_12*/
+
